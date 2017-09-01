90257855e8b87e7136a8533067ca8fe9b653e827
conyanco <n.namatame@gmail.com>
2017-09-01 20:36:56 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -7,31 +7,29 @@ const router = express.Router();
 const http = require('http');
 const url = 'http://shopping.yahooapis.jp/ShoppingWebService/V1/json/itemSearch?query=fashion&category_id=13457&type=all&image_size=300&affiliate_type=vc&affiliate_id=http%3a%2f%2fck%2ejp%2eap%2evaluecommerce%2ecom%2fservlet%2freferral%3fsid%3d3103511%26pid%3d882391693%26vc_url%3d&appid=dj0zaiZpPUZCV2h2YUZxdnZ5SCZzPWNvbnN1bWVyc2VjcmV0Jng9NmQ-';
 
-
 http.get(url, (res) => {
   let body = '';
   res.setEncoding('utf8');
-
   res.on('data', (chunk) => {
-      body += chunk;
+    body += chunk;
   });
-
   res.on('end', (res) => {
-      res = JSON.parse(body);
-let reskey = object.key(res);
-console.log(reskey);           
+    res = JSON.parse(body);
+    let reskey = object.key(res);
+    console.log(reskey);           
 //Object.keys(res).forEach(function(key){
 //      let value = this[key];
 //      console.log(key, value);},res);
  //     console.log(res);
 //      console.log(res.body);
-}).on('error', (e) => {
-  console.log(e.message);
+  }).on('error', (e) => {
+    console.log(e.message);
+  });
 });
 
 
 /* GET home page. */
-router.get('/', function(req, res) {
+  router.get('/', function(req, res) {
   res.render('index', { title: 'Express',key: value});
 });
 
