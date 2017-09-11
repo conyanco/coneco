3979c5df40a91b428e2a747a6d232828997fcf6b
conyanco <n.namatame@gmail.com>
2017-09-12 08:08:52 +0900

    app

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var search = require('./routes/search');
+
+var call = require('./routes/app');
 var app = express();
 
 // view engine setup
@@ -22,7 +24,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+//app.use('/', index);
+app.use('/', call);
 app.use('/users', users);
 app.use('/search', search);
 
--- routes/app.js
+++ routes/app.js
@@ -4,15 +4,16 @@ const express = require("express");
 const app = express();
 const http = require('http');
 const base_url = 'http://shopping.yahooapis.jp/ShoppingWebService/V1/json/itemSearch?&type=all&image_size=300&affiliate_type=vc&affiliate_id=http%3a%2f%2fck%2ejp%2eap%2evaluecommerce%2ecom%2fservlet%2freferral%3fsid%3d3103511%26pid%3d882391693%26vc_url%3d&appid=dj0zaiZpPUZCV2h2YUZxdnZ5SCZzPWNvbnN1bWVyc2VjcmV0Jng9NmQ-';
+let category = '13457';
 
-let url = base_url + keyword + category;
+let url = base_url + keyword + '&category_id=' + category;
 
 /*query=fashion&category_id=13457*/
 
 /* 2. listen()メソッドを実行して3000番ポートで待ち受け。*/
-let server = app.listen(3000, function(){
+/*let server = app.listen(3000, function(){
     console.log("Node.js is listening to PORT:" + server.address().port);
-});
+});*/
 
 /* 3. 以後、アプリケーション固有の処理 */
 
@@ -33,22 +34,23 @@ var photoList = [
 ]
 */
 // 写真リストを取得するAPI
-app.get("/call/", function(req, res, next){
-    res.json(base_url);
+//let result = '';
+app.get(url, function(req, res, next){
+    res.json(url);
 });
 
-app.get("/call/:keyword", function(req, res, next){
-    var result;
+app.get(url:keyword, function(req, res, next){
+    let result;
     for (i = 0; i < keyword.length; i++){
-        if (photoList[i].id == req.params.photoId){
-            var result = keyword[i];
+        if (url[i].id == req.params.keyword){
+            let result = keyword[i];
         }
     }
     res.json(result);
 });
 
 // View EngineにEJSを指定。
-app.set('view engine', 'ejs');
+//app.set('view engine', 'ejs');
 
 // "/"へのGETリクエストでindex.ejsを表示する。拡張子（.ejs）は省略されていることに注意。
 app.get("/", function(req, res, next){
